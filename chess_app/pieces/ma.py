0c7c14e83f6a6deab5fd3f89f9f46c13ad3849eb
Blue084-end <azend11110000@gmail.com>
2025-09-06 19:11:45 +0700

    Update ma.py

--- chess_app/pieces/ma.py
+++ chess_app/pieces/ma.py
@@ -1,1 +1,28 @@
+# pieces/ma.py
+
+from pieces.base import Piece
+
+class Ma(Piece):
+    def get_valid_moves(self, board):
+        x, y = self.position
+        moves = []
+
+        # Các hướng đi của mã
+        directions = [
+            ((-2, -1), (-1, 0)), ((-2, 1), (-1, 0)),
+            ((2, -1), (1, 0)), ((2, 1), (1, 0)),
+            ((-1, -2), (0, -1)), ((1, -2), (0, -1)),
+            ((-1, 2), (0, 1)), ((1, 2), (0, 1))
+        ]
+
+        for (dx, dy), (block_x, block_y) in directions:
+            bx, by = x + block_x, y + block_y
+            tx, ty = x + dx, y + dy
+            if 0 <= tx < 10 and 0 <= ty < 9:
+                if board.grid[bx][by] is None:
+                    target = board.grid[tx][ty]
+                    if target is None or target.color != self.color:
+                        moves.append((tx, ty))
+
+        return moves
 
