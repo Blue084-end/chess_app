5e8cabf77e06670717fa70ffd8654ccf42dc14b5
Blue084-end <azend11110000@gmail.com>
2025-09-06 18:57:15 +0700

    Update board.py

--- chess_app/board.py
+++ chess_app/board.py
@@ -1,1 +1,13 @@
+class Board:
+    def __init__(self):
+        self.grid = [[None for _ in range(9)] for _ in range(10)]
+        self.setup_initial_position()
+
+    def setup_initial_position(self):
+        # Đặt quân vào vị trí khai cuộc
+        pass
+
+    def move_piece(self, from_pos, to_pos):
+        # Di chuyển quân, kiểm tra hợp lệ
+        pass
 
