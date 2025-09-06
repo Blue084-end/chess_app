da340798fdca534fe3601270396a83630a638388
Blue084-end <azend11110000@gmail.com>
2025-09-06 19:19:33 +0700

    Update tot.py

--- chess_app/pieces/tot.py
+++ chess_app/pieces/tot.py
@@ -1,1 +1,28 @@
+# pieces/tot.py
+
+from pieces.base import Piece
+
+class Tot(Piece):
+    def get_valid_moves(self, board):
+        x, y = self.position
+        moves = []
+
+        # Đi thẳng
+        dx = -1 if self.color == "black" else 1
+        nx = x + dx
+        if 0 <= nx < 10:
+            target = board.grid[nx][y]
+            if target is None or target.color != self.color:
+                moves.append((nx, y))
+
+        # Qua sông thì được đi ngang
+        if (self.color == "red" and x < 5) or (self.color == "black" and x > 4):
+            for dy in [-1, 1]:
+                ny = y + dy
+                if 0 <= ny < 9:
+                    target = board.grid[x][ny]
+                    if target is None or target.color != self.color:
+                        moves.append((x, ny))
+
+        return moves
 
