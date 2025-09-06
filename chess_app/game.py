948bdf6d0186c32a068a658ebe301f5af620daa7
Blue084-end <azend11110000@gmail.com>
2025-09-06 18:57:53 +0700

    Update game.py

--- chess_app/game.py
+++ chess_app/game.py
@@ -1,1 +1,15 @@
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.turn = "red"
+        self.history = []
+
+    def make_move(self, from_pos, to_pos):
+        # Kiểm tra hợp lệ, cập nhật bàn cờ
+        pass
+
+    def restart(self):
+        self.board.setup_initial_position()
+        self.turn = "red"
+        self.history.clear()
 
