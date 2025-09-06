6fbe460378fbfc6369155b9de5aea2aced7a9716
Blue084-end <azend11110000@gmail.com>
2025-09-06 19:03:13 +0700

    Update utils.py

--- chess_app/utils.py
+++ chess_app/utils.py
@@ -1,1 +1,10 @@
+# utils.py
+
+def parse_position(pos_str):
+    try:
+        x, y = map(int, pos_str.strip().split(","))
+        if 0 <= x < 10 and 0 <= y < 9:
+            return (x, y)
+    except:
+        return None
 
