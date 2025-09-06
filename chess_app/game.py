0f5bb5817ecb9a55e22c05f949fd00ab6c148259
Blue084-end <azend11110000@gmail.com>
2025-09-06 19:02:31 +0700

    Update game.py

--- chess_app/game.py
+++ chess_app/game.py
@@ -13,3 +13,31 @@ class Game:
         self.turn = "red"
         self.history.clear()
 
+# game.py
+
+from board import Board
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.turn = "red"
+        self.history = []
+
+    def make_move(self, from_pos, to_pos):
+        fx, fy = from_pos
+        piece = self.board.get_piece(fx, fy)
+        if piece and piece.color == self.turn:
+            moved = self.board.move_piece(from_pos, to_pos)
+            if moved:
+                self.history.append((from_pos, to_pos))
+                self.switch_turn()
+                return True
+        return False
+
+    def switch_turn(self):
+        self.turn = "black" if self.turn == "red" else "red"
+
+    def restart(self):
+        self.board = Board()
+        self.turn = "red"
+        self.history.clear()
