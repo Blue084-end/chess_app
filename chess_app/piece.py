967afa474d9a9ef655a88ba8374eca60acee8b57
Blue084-end <azend11110000@gmail.com>
2025-09-06 18:56:22 +0700

    Update piece.py

--- chess_app/piece.py
+++ chess_app/piece.py
@@ -1,1 +1,10 @@
+class Piece:
+    def __init__(self, name, color, position):
+        self.name = name      # "xe", "ma", "tuong"...
+        self.color = color    # "red" hoặc "black"
+        self.position = position  # (x, y)
+
+    def get_valid_moves(self, board):
+        # Trả về danh sách nước đi hợp lệ
+        pass
 
