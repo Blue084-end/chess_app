eaf9e450975790f863cad28da24d447d9ae608a9
Blue084-end <azend11110000@gmail.com>
2025-09-06 19:00:24 +0700

    Update piece.py

--- chess_app/piece.py
+++ chess_app/piece.py
@@ -7,4 +7,18 @@ class Piece:
     def get_valid_moves(self, board):
         # Trả về danh sách nước đi hợp lệ
         pass
+# piece.py
+
+class Piece:
+    def __init__(self, name, color, position):
+        self.name = name          # "xe", "ma", "tuong", "si", "phao", "tot"
+        self.color = color        # "red" hoặc "black"
+        self.position = position  # (x, y)
+
+    def get_valid_moves(self, board):
+        # Placeholder: mỗi quân sẽ có logic riêng
+        return []
+
+    def __repr__(self):
+        return f"{self.color}_{self.name}@{self.position}"
 
