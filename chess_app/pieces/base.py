7f641bc5ea74b5393f4cc87d6a74c1d23de637d8
Blue084-end <azend11110000@gmail.com>
2025-09-06 19:09:57 +0700

    Update base.py

--- chess_app/pieces/base.py
+++ chess_app/pieces/base.py
@@ -1,1 +1,11 @@
+# pieces/base.py
+
+class Piece:
+    def __init__(self, name, color, position):
+        self.name = name
+        self.color = color
+        self.position = position
+
+    def get_valid_moves(self, board):
+        return []
 
